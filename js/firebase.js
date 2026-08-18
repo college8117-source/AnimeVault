741f66328ac0a5927236c3c076842347470c77a3
Raj <college8117@gmail.com>
2026-08-18 15:00:20 +0530

    Add Firebase frontend configuration

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBJZPJXu0PWo_JGZY7ohA7m4SVEgp_wEig",
+  authDomain: "animevault-1.firebaseapp.com",
+  projectId: "animevault-1",
+  storageBucket: "animevault-1.firebasestorage.app",
+  messagingSenderId: "879320021973",
+  appId: "1:879320021973:web:ba236a7386e136d0451b5c",
+};
+
+export { firebaseConfig };
